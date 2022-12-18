e47336960514240e5b0936786314adc664aa6575
aymane <aymensarrar1@gmail.com>
2022-12-18 21:11:46 +0100

    feat(header): adding illustrations and animations

--- components/Header/Content.tsx
+++ components/Header/Content.tsx
@@ -1,21 +1,39 @@
 import { FiDownload } from "react-icons/fi";
+import { motion } from "framer-motion";
 const Content = () => {
   return (
-    <div className="flex flex-col items-center justify-center flex-1 max-w-3xl gap-4 mx-auto text-center text-white ">
-      <h1 className="text-5xl font-bold">IMAGINE A PLACE...</h1>
-      <p>
+    <div className="flex flex-col items-start justify-start flex-1 max-w-3xl gap-4 p-2 mx-auto text-center text-white md:items-center md:justify-center">
+      <motion.h1
+        initial={{ y: -50 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 2 }}
+        className="text-5xl font-bold"
+      >
+        IMAGINE A PLACE...
+      </motion.h1>
+      <p className="text-left md:text-center">
         ...where you can belong to a school club, a gaming group, or a worldwide
         art community. Where just you and a handful of friends can spend time
         together. A place that makes it easy to talk every day and hang out more
         often.
       </p>
       <div className="flex flex-wrap gap-2">
-        <button className="bg-white text-[#23272a] px-6 py-4 rounded-full text-xl flex items-center justify-center gap-1 hover:text-[#5865f2] transition-all duration-150">
+        <motion.button
+          initial={{ x: -100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2 }}
+          className="bg-white text-[#23272a] px-6 py-4 rounded-full text-xl flex items-center justify-center gap-1 hover:text-[#5865f2] transition-all duration-150"
+        >
           <FiDownload className="font-bold" /> Download for Linux
-        </button>
-        <button className="bg-[#23272a] text-white px-6 py-4 rounded-full text-xl hover:shadow-xl hover:bg-[hsl(220,7.7%,22.9%)]">
+        </motion.button>
+        <motion.button
+          initial={{ x: 100 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 2 }}
+          className="bg-[#23272a] text-white px-6 py-4 rounded-full text-xl hover:shadow-xl hover:bg-[hsl(220,7.7%,22.9%)]"
+        >
           Open Discord in your browser
-        </button>
+        </motion.button>
       </div>
     </div>
   );
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Content from "./Content";
+import LeftIllustration from "./LeftIllustration";
 import Navbar from "./Navbar";
+import RightIllustration from "./RightIllustration";
 
 const Header = () => {
   return (
-    <header className="flex flex-col min-h-screen bg-[#404eed] relative">
+    <header className="flex flex-col min-h-screen bg-[#404eed] relative overflow-hidden">
       <Navbar />
       <Content />
+      <LeftIllustration
+        initial={{ opacity: 0 }}
+        animation={{ opacity: 1 }}
+        className="invisible sm:visible absolute bottom-0 -left-[20rem]"
+      />
+      <RightIllustration
+        initial={{ opacity: 0 }}
+        animation={{ opacity: 1 }}
+        className="absolute bottom-0 -right-[20rem]"
+      />
     </header>
   );
 };
