e1ae11bb1e24b40e2350ef4002f4e052039902fb
aymane <aymensarrar1@gmail.com>
2022-12-26 19:34:02 +0100

    refactor(input): fix undefined data

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,16 +1,33 @@
+import { UseFormRegister } from "react-hook-form";
+import { Inputs } from "../Register/Form";
+
 interface InputProps {
   label: string;
   type?: string;
   id: string;
+  value?: string;
+  register: UseFormRegister<Inputs>;
+  required?: boolean;
+  inputLabel: "email" | "username" | "password" | "confirmPassword";
 }
 
-const Input: React.FC<InputProps> = ({ label, type, id }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  type,
+  id,
+  value,
+  register,
+  required,
+  inputLabel,
+}) => {
   return (
     <label htmlFor={id} className="flex flex-col gap-2 mt-2">
       {label}
       <input
+        {...register(inputLabel, { required })}
         type={type}
         id={id}
+        value={value}
         className="outline-none text-white bg-[#2F3338] px-4 py-2 rounded-sm focus:border-[1px] focus:border-[#748AD6]"
       />
     </label>
--- components/Register/Form.tsx
+++ components/Register/Form.tsx
@@ -1,14 +1,49 @@
 import Input from "../Input/Input";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+export interface Inputs {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
+}
 const Form = () => {
+  const { register, handleSubmit, watch } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="grid  grid-cols-1 bg-[#35393E] px-4 py-10 w-full md:w-1/2 text-[#737479]">
       <h1 className="text-xl text-center text-white">Create an account</h1>
-      <form>
-        <Input label="E-mail" type="email" id="email" />
-        <Input label="Username" type="text" id="username" />
-        <Input label="Password" type="password" id="password" />
-        <Input label="Confirm Password" type="password" id="confirmpassword" />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          register={register}
+          inputLabel="email"
+          label="E-mail"
+          type="email"
+          id="email"
+        />
+        <Input
+          register={register}
+          inputLabel="username"
+          label="Username"
+          type="text"
+          id="username"
+        />
+        <Input
+          register={register}
+          inputLabel="password"
+          label="Password"
+          type="password"
+          id="password"
+        />
+        <Input
+          register={register}
+          inputLabel="confirmPassword"
+          label="Confirm Password"
+          type="password"
+          id="confirmpassword"
+        />
 
         <button className="bg-[#748AD6] text-white px-4 py-2 w-full mt-4">
           register
