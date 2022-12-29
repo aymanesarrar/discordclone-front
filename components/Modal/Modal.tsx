eb79ba31f5125581bffffb3e144974d1652b93d5
aymane <aymensarrar1@gmail.com>
2022-12-29 18:06:08 +0100

    feat(recoil): adding recoil root on the root of the app

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,4 @@
+const Modal: React.FC<IModalProps> = ({ type, message }) => {
+  return <div></div>;
+};
+export { Modal as default };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />;
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
